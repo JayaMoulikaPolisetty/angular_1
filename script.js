20f7b132c9c172fc854e55a7de56beacdc381a65
mouli <mouli>
2016-02-15 17:19:32 +0530

    project

---
+++ script.js
@@ -0,0 +1,40 @@
+var app = angular.module("source",['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+	
+	when('/main',{
+
+			templateUrl: 'main.html',
+			controller: 'MainCtrl'
+	}).
+	when('/about',{
+
+			templateUrl: 'about.html',
+			controller: 'MainCtrl'
+	}).
+	when('/services',{
+
+			templateUrl: 'services.html',
+			controller: 'ServicesCtrl'
+	}).
+	when('/contact',{
+
+			templateUrl: 'contact.html',
+			controller: 'ContactCtrl'
+	}).
+	otherwise({redirectTo:'/main'})
+
+}])
+
+.controller('MainCtrl', ['$scope', function($scope){
+	
+}])
+
+.controller('ServicesCtrl', ['$scope', function($scope){
+	
+	
+}])
+.controller('ContactCtrl', ['$scope', function($scope){
+	
+}]);
